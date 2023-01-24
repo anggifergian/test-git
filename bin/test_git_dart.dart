c15db3d86277ef8f6ab34909362d28395b350eeb
Anggi Fergian <anggi.fergian@koinworks.com>
2023-01-24 12:36:38 +0700

    first commit

--- bin/test_git_dart.dart
+++ bin/test_git_dart.dart
@@ -1,5 +1,3 @@
-import 'package:test_git_dart/test_git_dart.dart' as test_git_dart;
-
 void main(List<String> arguments) {
-  print('Hello world: ${test_git_dart.calculate()}!');
+  print('My First Commit');
 }
