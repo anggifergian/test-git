4acf8106815b8b153ff4f736a23abd6c32fe004a
Anggi Fergian <anggi.fergian@koinworks.com>
2023-01-24 12:34:58 +0700

    Initial commit

---
+++ bin/test_git_dart.dart
@@ -0,0 +1,5 @@
+import 'package:test_git_dart/test_git_dart.dart' as test_git_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_git_dart.calculate()}!');
+}
---
+++ test/test_git_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_git_dart/test_git_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
